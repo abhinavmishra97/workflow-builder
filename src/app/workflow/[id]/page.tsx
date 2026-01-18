569a565574ca4df007c17f337a1ad9eb301de57e
abhinavmishra97 <mishraabhinav2306@gmail.com>
2026-01-18 19:46:36 +0530

    build issue fix

--- src/app/workflow/[id]/page.tsx
+++ src/app/workflow/[id]/page.tsx
@@ -3,19 +3,20 @@ import { redirect } from "next/navigation";
 import WorkflowCanvas from "@/components/WorkflowCanvas";
 
 interface WorkflowPageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
 export default async function WorkflowPage({ params }: WorkflowPageProps) {
   const { userId } = await auth();
+  const { id } = await params;
 
   if (!userId) {
     redirect("/sign-in");
   }
 
+  // pass the ID to the canvas if needed, or just let it load
   // For now, just render the canvas
-  // TODO: Load workflow data from database using params.id
   return <WorkflowCanvas />;
 }
